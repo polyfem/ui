ed4db851fed52dc728bbd8a4369afdbbc163402a
nikkylu <nikkylu@foxmail.com>
2022-04-29 22:24:50 -0400

    feat: ts setup

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+var message = 'Hello, TypeScript!';
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
---
+++ src/js/main.ts
@@ -0,0 +1,6 @@
+let message: string = 'Hello, TypeScript!';
+
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+document.body.appendChild(heading);
